4e6cd53143c40bd37bf36455c69aae7b60917f5e
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2022-03-03 00:55:25 +0000

    Style: Creado archivo Colors.js con paleta de colores a emplear.

---
+++ constants/Colors.js
@@ -0,0 +1,13 @@
+const darkBlue = '#373A58'
+const lightBlue = '#565A8A'
+const beige = '#E9EDDE'
+const grey = '#3D3B30'
+const yellow = '#E7E247'
+
+export default {
+    background: darkBlue,
+    primary: lightBlue,
+    secondary: beige,
+    accent1: grey,
+    accent2: yellow
+}
