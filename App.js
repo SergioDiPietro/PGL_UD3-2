e26e67687c3c7d9f24b582c1fc1428d6af52ecff
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2022-03-04 08:55:37 +0000

    Feat: Creados views principales que contendrán los componentes.

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Colors from './constants/Colors';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={styles.balanceContainer}></View>
+      <View style={styles.transactionListContainer}>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: Colors.background,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 40
   },
+  balanceContainer: {
+    flex: 1,
+    borderRadius: 10,
+    width: '95%'
+  }, 
+  transactionListContainer: {
+    flex: 4,
+    backgroundColor: Colors.primary,
+    borderRadius: 10,
+    width: '95%'
+  }
 });
